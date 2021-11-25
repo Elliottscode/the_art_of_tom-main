986b45418e88633a65b27a4f2ba8662335727e81
Elliottscode <elliottmccully@yahoo.com>
2021-11-25 17:18:16 +0000

    Adding routeing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-export function App() {
+
+import Home from "./src/pages/Home";
+
+import About from "./src/pages/About";
+// import Home {Banner} from "./pages/Home";
+
+
+
+export function App () {
     return (
+      <Router>
         <div>
-            <h1>still working{new Date().toString()}</h1>
+            <Link to="/">Home</Link>
+            
+            <Link to="/">About</Link>
         </div>
+        <Routes>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" exact>
+            <About />
 
+
+          </Route>
+
+        </Routes>
+      </Router>
     );
 }
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Home = () => {
+
+    return (
+      <div>
+        <h1>Home</h1>
+        <div>Here is stuff</div>
+      </div>
+  
+    );
+  
+};
+
+
+export default Home;
