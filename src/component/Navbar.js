2360b8d07761ae3f2d958c67249a49cff78b0656
Elliottscode <elliottmccully@yahoo.com>
2022-01-06 17:06:32 +0000

    nav fix

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './Navbar.scss'
 import openIcon from'../assets/images/open.png'
 
@@ -16,11 +16,11 @@ export default class Navbar extends React.Component{
         return (
                 <nav className="Navbar" >
                     <ul>
-                        <Link className="text-link" to="/" >Home</Link>
-                        <Link className="text-link" to="/about" >About</Link>
-                        <Link className="text-link" to="/faq" >FAQ</Link>
-                        <Link className="text-link" to="/contact" >Contact</Link>
-                        <Link className="text-link" to="/support" >Support</Link>
+                        <NavLink className="text-link" to="/" >Home</NavLink>
+                        <NavLink activeClassName="activeLink" className="text-link" to="/about" >About</NavLink>
+                        <NavLink activeClassName="activeLink" className="text-link" to="/faq" >FAQ</NavLink>
+                        <NavLink activeClassName="activeLink" className="text-link" to="/contact" >Contact</NavLink>
+                        <NavLink activeClassName="activeLink" className="text-link" to="/support" >Support</NavLink>
                     </ul>
                 </nav>
         );
