4b28c61861758e10f7d75fd077ab99f9bf6aabc4
Elliottscode <elliottmccully@yahoo.com>
2022-09-08 11:32:41 +0100

    Create testCategory.jsx

---
+++ src/component/slider/testCategory.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from "react";
+
+
+
+// swiper component import 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+// swiper styles 
+import "swiper/scss";
+import "swiper/scss/pagination";
+
+
+// my styles 
+import "./sliderStyles.scss"
+
+// art data 
+import  artData from "../../assets/art/imageindex";
+
+import SwiperCore, { Pagination } from "swiper";
+
